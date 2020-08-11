24f41c39398e1df7d1c57dc6e8fa9bec0d073cb7
Jack Lavender <jack@lavnet.net>
2020-08-10 21:10:00 -0500

    fix grafana server, attempt to bootstrap ansible (failed)

--- bin/init_ssh.sh
+++ bin/init_ssh.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+sudo apt install ansible -y
+sudo apt install ssh-askpass -y
+
 mkdir -p ~/.ssh
 ssh-keygen -t rsa -f ~/.ssh/id_rsa -q -N ""
 cp ~/.ssh/id_rsa.pub ~/.ssh/authorized_keys
