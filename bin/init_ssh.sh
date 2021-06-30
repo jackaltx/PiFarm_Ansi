7d60de2b43adf7fb87da94a5d88feae0ee8e9acc
jackaltx <6139840+jackaltx@users.noreply.github.com>
2021-06-30 18:01:22 -0500

    Update init_ssh.sh
    tailoring for Centos 8

--- bin/init_ssh.sh
+++ bin/init_ssh.sh
@@ -3,8 +3,12 @@
 # Used to setup the local machine for the "farmer" role
 # This is used one time only.  I did nothing to make it idempotent
 
-sudo apt install ansible -y
-sudo apt install ssh-askpass -y
+# TODO...determine the distro and install accordingly
+
+# this will be centos  ssh-ask only for now
+sudo dnf install centos-release-ansible-29 openssh-askpass -y
+#sudo dns install ansible -y
+#sudo apt install ssh-askpass -y
 
 mkdir -p ~/.ssh
 ssh-keygen -t rsa -f ~/.ssh/id_rsa -q -N ""
