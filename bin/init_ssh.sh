716c13562448e66b21136814c1299d50c5213d24
jackaltx <jack@lavnet.net>
2021-07-16 20:03:53 -0500

    merge bin changes

--- bin/init_ssh.sh
+++ bin/init_ssh.sh
@@ -3,8 +3,14 @@
 # Used to setup the local machine for the "farmer" role
 # This is used one time only.  I did nothing to make it idempotent
 
-sudo apt install ansible -y
-sudo apt install ssh-askpass -y
+# TODO...determine the distro and install accordingly
+
+# ... RHEL family
+sudo dnf install ansible openssh-askpass -y
+
+# ... Debian
+# sudo apt install ansible -y
+# sudo apt install ssh-askpass -y
 
 mkdir -p ~/.ssh
 ssh-keygen -t rsa -f ~/.ssh/id_rsa -q -N ""
